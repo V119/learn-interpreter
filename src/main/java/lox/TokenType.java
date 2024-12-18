2995619609da850d561985ba46fac1fcfcddd0c6
DeMH <928131755@qq.com>
2024-12-19 01:33:33 +0800

    complete scanning

---
+++ src/main/java/lox/TokenType.java
@@ -0,0 +1,22 @@
+package lox;
+
+public enum TokenType {
+    // single-character tokens
+    LEFT_PARENT, RIGHT_PARENT, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPPER, THIS, TRUE, VAR, WHILE, SUPER,
+
+    EOF
+}
